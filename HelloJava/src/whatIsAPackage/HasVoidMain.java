9d05f230f69858bb145b4a5f1b7d6f01ca530746
MinYenYimYinYen <lukeholker@gmlawncare.onmicrosoft.com>
2018-12-08 21:44:59 -0600

    Started a HelloWorld. Created a Person class. Instaniated a Person, named it, and wrote name to output.

---
+++ HelloJava/src/whatIsAPackage/HasVoidMain.java
@@ -0,0 +1,17 @@
+package whatIsAPackage;
+
+public class HasVoidMain {
+
+	public static void main(String[] args) {
+
+		Person person = new Person();
+		person.FirstName = "Whutda";
+		person.LastName = "Fuckizit";
+		
+		System.out.println(person.FullName());
+		System.out.println("Who knows about these people...");
+		
+	}
+
+}
+
---
+++ HelloJava/src/whatIsAPackage/Person.java
@@ -0,0 +1,16 @@
+package whatIsAPackage;
+
+
+public class Person {
+	
+	public String FirstName;
+	public String LastName;
+	
+	public String FullName(){
+		return FirstName + ' ' + LastName;
+	}
+	
+	
+	
+
+}
